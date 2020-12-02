5dad56ed82eacd07112137c282786894b6fad35d
Andrey Yelmanov <andrey@Andreys-MacBook-Pro.local>
2020-12-02 19:44:52 +0300

    Initial commit

---
+++ src/main/java/ru/digitalleague/spacex/domain/Ship.java
@@ -0,0 +1,46 @@
+package ru.digitalleague.spacex.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ship")
+public class Ship {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "status")
+    private String status;
+
+    public Ship() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
